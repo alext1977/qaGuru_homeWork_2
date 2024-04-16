1bbda79416daf493345f449ca2f2d0ee273a8897
atitomirov <burjui777@yandex.ru>
2024-04-16 08:30:40 +0300

    1.jpg added

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -8,6 +8,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+
         //Configuration.holdBrowserOpen = true;
 
     }
